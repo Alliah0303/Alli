540c6d954feba9f98a1b06eb319bfa0734b0fdaa
Alliah <salapantan.judealliahsit2011@gmail.com>
2024-10-18 20:34:48 +0700

    add

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,15 +1,15 @@
 import '../pages/style.css'
 
-import card2 from '../images/Card 2.svg'
-import card1 from '../images/Card 1.svg'
-import card3 from '../images/Card 3.svg'
+import card2 from '../images/Thumbnail.svg'
+import card1 from '../images/Thumbnail3.svg'
+import card3 from '../images/Thumbnail2.svg'
 
 function Project() {
     return (
       <div className='Card'>
         <ul >
-            <li className='cards'><img src={card2} ></img></li> 
-            <li className='cards'><img src={card1} ></img></li>
+            <li className='cards'><img src={card1} ></img></li> 
+            <li className='cards'><img src={card2} ></img></li>
             <li className='cards'><img src={card3} ></img></li>
         </ul>
       </div>
