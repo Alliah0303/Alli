018472f121b6665a5c2e15ad8429c5c4e6705b07
Alliah <salapantan.judealliahsit2011@gmail.com>
2024-10-22 19:40:34 +0700

    added

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,8 +9,11 @@ function Project() {
       <div className='Card'>
         <ul >
             <li className='cards'><img src={card1} ></img></li> 
+            <p className='descrip'> My project, called "Awesome Todos," ia a website using React for the front-end, which provides a dynamic and responsive user interface, and MongoDB for the back-end, which serves as the database to store and manage user tasks efficiently.  </p>
             <li className='cards'><img src={card2} ></img></li>
+            <p className='descrip'>  My project is a furniture selling website built using HTML, CSS, and JavaScript, designed to provide a visually appealing and interactive platform for users to browse and purchase furniture products.</p>
             <li className='cards'><img src={card3} ></img></li>
+            <p className='descrip'> The project "Nexcent" is a sophisticated and dynamic website UI designed specifically for agencies. It utilizes vanilla HTML for structure, Tailwind CSS for styling, and JavaScript for interactivity, ensuring a seamless and visually appealing user experience. The combination of these technologies allows for a responsive and modern design that can effectively showcase agency services and engage users. </p>
         </ul>
       </div>
     );
